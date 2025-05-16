53b66d8b5c053a76fb99338983f2ec5d3a2b8a24
K B <kalyanbtl@gmail.com>
2025-05-15 21:29:45 -0400

    updated script

--- FCNVMB_train.py
+++ FCNVMB_train.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Fully Convolutional neural network (U-Net) for velocity model building from prestack
-
 unmigrated seismic data directly
-
-
-
-Created on Feb 2018
-
-@author: fangshuyang (yfs2016@hit.edu.cn)
-
 """
 
 ################################################
@@ -20,153 +12,128 @@ from ParamConfig import *
 from PathConfig import *
 from LibConfig import *
 
+from torch.profiler import profile, record_function, ProfilerActivity
+enable_profiling = True
+
 ################################################
 ########             NETWORK            ########
 ################################################
 
-# Here indicating the GPU you want to use. if you don't have GPU, just leave it.
 cuda_available = torch.cuda.is_available()
-device         = torch.device("cuda" if cuda_available else "cpu")
+device = torch.device("cuda" if cuda_available else "cpu")
 
-# net = UnetModel(n_classes=Nclasses,in_channels=Inchannels,is_deconv=True,is_batchnorm=True) 
-
-# net = torch.compile(net, mode="max-autotune", backend="inductor")
-
-# #net = torch.compile(net)             # Compile the model
-# net = net.to(device)                 # Move the compiled model to CPU or GPU
-
-net = UnetModel(n_classes=Nclasses,in_channels=Inchannels,is_deconv=True,is_batchnorm=True) 
+net = UnetModel(n_classes=Nclasses, in_channels=Inchannels, is_deconv=True, is_batchnorm=True) 
 if torch.cuda.is_available():
     net.cuda()
 
-# Optimizer we want to use
-optimizer = torch.optim.Adam(net.parameters(),lr=LearnRate)
+optimizer = torch.optim.Adam(net.parameters(), lr=LearnRate)
 
-# if torch.cuda.is_available():
-#     net.cuda()
-
-# Optimizer we want to use
-optimizer = torch.optim.Adam(net.parameters(),lr=LearnRate)
-
-# If ReUse, it will load saved model from premodelfilepath and continue to train
 if ReUse:
     print('***************** Loading the pre-trained model *****************')
     print('')
     premodel_file = models_dir + premodelname + '.pkl'
-    ##Load generator parameters
-    net  = net.load_state_dict(torch.load(premodel_file))
-    net  = net.to(device)
-    print('Finish downloading:',str(premodel_file))
-    
+    net = net.load_state_dict(torch.load(premodel_file))
+    net = net.to(device)
+    print('Finish downloading:', str(premodel_file))
+
 ################################################
 ########    LOADING TRAINING DATA       ########
 ################################################
 print('***************** Loading Training DataSet *****************')
-print("----TrainSize ------", TrainSize)
-train_set,label_set,data_dsp_dim,label_dsp_dim  = DataLoad_Train(train_size=TrainSize,train_data_dir=train_data_dir, \
-                                                                 data_dim=DataDim,in_channels=Inchannels, \
-                                                                 model_dim=ModelDim,data_dsp_blk=data_dsp_blk, \
-                                                                 label_dsp_blk=label_dsp_blk,start=1, \
-                                                                 datafilename=datafilename,dataname=dataname, \
-                                                                 truthfilename=truthfilename,truthname=truthname)
-# Change data type (numpy --> tensor)
-train        = data_utils.TensorDataset(torch.from_numpy(train_set),torch.from_numpy(label_set))
-train_loader = data_utils.DataLoader(train,batch_size=BatchSize,shuffle=True)
+train_set, label_set, data_dsp_dim, label_dsp_dim = DataLoad_Train(
+    train_size=TrainSize,
+    train_data_dir=train_data_dir,
+    data_dim=DataDim,
+    in_channels=Inchannels,
+    model_dim=ModelDim,
+    data_dsp_blk=data_dsp_blk,
+    label_dsp_blk=label_dsp_blk,
+    start=1,
+    datafilename=datafilename,
+    dataname=dataname,
+    truthfilename=truthfilename,
+    truthname=truthname
+)
 
-
+train = data_utils.TensorDataset(torch.from_numpy(train_set), torch.from_numpy(label_set))
+train_loader = data_utils.DataLoader(train, batch_size=BatchSize, shuffle=True)
 
 ################################################
 ########            TRAINING            ########
 ################################################
 
-print() 
-print('*******************************************') 
-print('*******************************************') 
-print('           START TRAINING                  ') 
-print('*******************************************') 
-print('*******************************************') 
-print() 
+print('\n' + '*' * 43)
+print('           START TRAINING')
+print('*' * 43 + '\n')
 
+print(f'Original data dimension: {DataDim}')
+print(f'Downsampled data dimension: {data_dsp_dim}')
+print(f'Original label dimension: {ModelDim}')
+print(f'Downsampled label dimension: {label_dsp_dim}')
+print(f'Training size: {TrainSize}')
+print(f'Training batch size: {BatchSize}')
+print(f'Number of epochs: {Epochs}')
+print(f'Learning rate: {LearnRate:.5f}')
 
-print ('Original data dimention:%s'      %  str(DataDim))
-print ('Downsampled data dimention:%s '  %  str(data_dsp_dim))
-print ('Original label dimention:%s'     %  str(ModelDim))
-print ('Downsampled label dimention:%s'  %  str(label_dsp_dim))
-print ('Training size:%d'                %  int(TrainSize))
-print ('Traning batch size:%d'           %  int(BatchSize))
-print ('Number of epochs:%d'             %  int(Epochs))
-print ('Learning rate:%.5f'              %  float(LearnRate))
-              
-# Initialization
-loss1  = 0.0
-step   = int(TrainSize/BatchSize)  ## depricated
-start  = time.time()
+loss1 = 0.0
+step = int(TrainSize / BatchSize)
+start = time.time()
 
 for epoch in range(Epochs): 
     epoch_loss = 0.0
-    since      = time.time()
-    for i, (images,labels) in enumerate(train_loader):        
-        iteration  = epoch*step+i+1
-        # Set Net with train condition
+    since = time.time()
+    for i, (images, labels) in enumerate(train_loader):        
+        iteration = epoch * step + i + 1
         net.train()
+
+        images = images.view(BatchSize, Inchannels, data_dsp_dim[0], data_dsp_dim[1]).to(device)
+        labels = labels.view(BatchSize, Nclasses, label_dsp_dim[0], label_dsp_dim[1]).to(device)
         
-        # Reshape data size
-        images = images.view(BatchSize,Inchannels,data_dsp_dim[0],data_dsp_dim[1])
-        labels = labels.view(BatchSize,Nclasses,label_dsp_dim[0],label_dsp_dim[1])
-        images = images.to(device)
-        labels = labels.to(device)
-        
-        # Zero the gradient buffer
         optimizer.zero_grad()     
-        
-        # Forward prediction
-        outputs = net(images,label_dsp_dim)
-        
-        # Calculate the MSE
-        loss    = F.mse_loss(outputs,labels,reduction='sum')/(label_dsp_dim[0]*label_dsp_dim[1]*BatchSize)
-        
+
+        # ========= PROFILING SECTION =========
+        if enable_profiling and epoch == 0 and i == 0:
+            with profile(
+                activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+                record_shapes=True, with_stack=True
+            ) as prof:
+                with record_function("model_inference"):
+                    outputs = net(images, label_dsp_dim)
+                    loss = F.mse_loss(outputs, labels, reduction='sum') / (label_dsp_dim[0]*label_dsp_dim[1]*BatchSize)
+            enable_profiling = False
+
+            print("\n[Profiler] --- Top CUDA Ops ---")
+            print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=20))
+
+        else:
+            outputs = net(images, label_dsp_dim)
+            loss = F.mse_loss(outputs, labels, reduction='sum') / (label_dsp_dim[0]*label_dsp_dim[1]*BatchSize)
+        # =======================================
+
         if np.isnan(float(loss.item())):
             raise ValueError('loss is nan while training')
-            
-        epoch_loss += loss.item()    
-        # Loss backward propagation    
+
+        epoch_loss += loss.item()
         loss.backward()
-        
-        # Optimize
         optimizer.step()
         
-        # Print loss
         if iteration % DisplayStep == 0:
-            print('Epoch: {}/{}, Iteration: {}/{} --- Training Loss:{:.6f}'.format(epoch+1, \
-                                                                               Epochs,iteration, \
-                                                                              step*Epochs,loss.item()))        
-        
-    # Print loss and consuming time every epoch
-    if (epoch+1) % 1 == 0:
-        #print ('Epoch [%d/%d], Loss: %.10f' % (epoch+1,Epochs,loss.item()))          
-        #loss1 = np.append(loss1,loss.item())
-        print('Epoch: {:d} finished ! Loss: {:.5f}'.format(epoch+1,epoch_loss/i))
-        loss1 = np.append(loss1,epoch_loss/i)
+            print('Epoch: {}/{}, Iteration: {}/{} --- Training Loss:{:.6f}'.format(
+                epoch + 1, Epochs, iteration, step * Epochs, loss.item()))
+
+    if (epoch + 1) % 1 == 0:
+        print(f'Epoch: {epoch+1} finished! Loss: {epoch_loss/i:.5f}')
+        loss1 = np.append(loss1, epoch_loss/i)
         time_elapsed = time.time() - since
-        print('Epoch consuming time: {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-        
-    # Save net parameters every 10 epochs
-    if (epoch+1) % SaveEpoch == 0:
-        torch.save(net.state_dict(),models_dir+modelname+'_epoch'+str(epoch+1)+'.pkl')
-        print ('Trained model saved: %d percent completed'% int((epoch+1)*100/Epochs))
-    
+        print(f'Epoch consuming time: {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
 
-# Record the consuming time
+    if (epoch + 1) % SaveEpoch == 0:
+        torch.save(net.state_dict(), models_dir + modelname + '_epoch' + str(epoch + 1) + '.pkl')
+        print(f'Trained model saved: {int((epoch+1)*100/Epochs)} percent completed')
+
 time_elapsed = time.time() - start
-print('Training complete in {:.0f}m  {:.0f}s' .format(time_elapsed //60 , time_elapsed % 60))
+print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
 
-# Save the loss
-font2  = {'family': 'Times New Roman',
-    'weight': 'normal',
-    'size': 17,
-    }
-font3 = {'family': 'Times New Roman',
-    'weight': 'normal',
-    'size': 21,
-    }
-SaveTrainResults(loss=loss1,SavePath=results_dir,font2=font2,font3=font3)
+font2 = {'family': 'Times New Roman', 'weight': 'normal', 'size': 17}
+font3 = {'family': 'Times New Roman', 'weight': 'normal', 'size': 21}
+SaveTrainResults(loss=loss1, SavePath=results_dir, font2=font2, font3=font3)
--- README.md
+++ README.md
@@ -1,34 +1,9 @@
-# Deep-learning inversion: A next-generation seismic velocity model building method
-<a href="https://zenodo.org/badge/latestdoi/204304907"><img src="https://zenodo.org/badge/204304907.svg" alt="DOI"></a>
+## Introduction
 
-This is the python implementation (PyTorch) of the deep leraning model for velocity model building in a surpervised approach. The [paper](https://library.seg.org/doi/10.1190/geo2018-0249.1) is  published on Geophysics. The arxiv version of the paper is availabel 
-[here](https://arxiv.org/abs/1902.06267). 
-
-Note that the arxiv version is a litlle different from the publishion, please refer to the official version.
-
-## Abstract
-
-We investigate a novel method based on the supervised deep fully convolutional neural network (FCN) for velocity-model building (VMB) directly from raw seismograms. Unlike the conventional inversion method based on physical models, the supervised deep-learning methods 
-are based on big-data training rather than prior-knowledge assumptions. One key characteristic of the deep-learning method is that it 
-can automatically extract multi-layer useful features without the need for human-curated activities and initial velocity setup. The 
-data-driven method usually requires more time during the training stage, and actual predictions take less time, with only seconds 
-needed. Therefore, the computational time of geophysical inversions, including real-time inversions, can be dramatically reduced once a good generalized network is built. 
-
-## Experimental Results
-With the goal of estimating velocity models using seismic data as inputs directly, the network needs to project seismic data from the data domain to the model domain. Our method contains two stages: the training process and the prediction process, as shown in the following figure:
-
-![Flowchart of the FCN-based inversion process](/images/schematic.png)
-
-We design the CNN based on the famous [U-Net architecture](https://link.springer.com/chapter/10.1007/978-3-319-24574-4_28). We extensively validate the proposed method on similate data and experimental data (i.e. [SEG salt model](https://wiki.seg.org/wiki/Open_data#SEG.2FEAGE_Salt_and_Overthrust_Models)). The following two figures show several visual resultes of our method compared with full waveform inversion (FWI). It denotes that our proposed method is generally feasible for velocity model building.
-
-![Comparisons of the velocity inversion (simulated models)](/images/simulateresult.png)
-
-![Comparisons of the velocity inversion (SEG salt models)](/images/SEGresult.png)
 
 ## Dataset
 
-For the training process, we generate the simulated velocity models and their corresponding measurement by solving the acoustic wave equation. We provide the synthetic and SEG salt velocity models for training and testing process which are included in [FCNVMB-data.zip](https://github.com/YangFangShu/FCNVMB-Deep-learning-based-seismic-velocity-model-building). Note that you should generate the corresponding seismic measurement by yourself. Once you have the data pairs, you can train the network as following. 
-
+For the training process, we generate the simulated velocity models and their corresponding measurement by solving the acoustic wave equation.
 
 ## Training & Testing
 
@@ -105,21 +80,4 @@ All of them can be installed via ```conda (anaconda)```, e.g.
 ```
 conda install pytorch torchvision torchaudio cudatoolkit=10.0 -c pytorch
 ```
-
-## Citation
-
-If you find the paper and the code useful in your research, please cite the paper:
-```
-@article{yang2019deep,
-  title={Deep-learning inversion: A next-generation seismic velocity model building method},
-  author={Yang, Fangshu and Ma, Jianwei},
-  journal={Geophysics},
-  volume={84},
-  number={4},
-  pages={R583--R599},
-  year={2019},
-  publisher={Society of Exploration Geophysicists}
-}
-```
-If you have any questions about this paper, feel free to contract us: yangfs@hit.edu.cn
 
